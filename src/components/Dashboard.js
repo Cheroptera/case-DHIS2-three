30884f64af7511b9a780a27e8eb0eda03bd0cd6c
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-11-10 22:27:08 +0100

    fixed test icons and conditional rendering of them

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,14 +4,19 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { ReactComponent as MapIcon } from '../assets/map.svg';
+import { ReactComponent as DiagramIcon } from '../assets/diagram.svg';
+import { ReactComponent as TableIcon } from '../assets/table.svg';
+import { ReactComponent as DataVisIcon } from '../assets/data-visualization.svg';
+import { ReactComponent as TextIcon } from '../assets/text.svg';
+import { ReactComponent as StatisticsIcon } from '../assets/statistics.svg';
 
-const Dashboard = ({ dashboard }) => {
-  const [expanded, setExpanded] = useState(false);
+const Dashboard = ({ dashboard, isExpanded, onDashboardClick }) => {
   const [dashboardDetails, setDashboardDetails] = useState(null);
 
   useEffect(() => {
     // Fetch specific dashboard details when expanded
-    if (expanded && dashboard && dashboard.id) {
+    if (isExpanded && dashboard && dashboard.id) {
       const url = `https://gist.githubusercontent.com/kabaros/da79636249e10a7c991a4638205b1726/raw/fa044f54e7a5493b06bb51da40ecc3a9cb4cd3a5/${dashboard.id}.json`;
 
       console.log('Fetching dashboard details for URL:', url);
@@ -33,33 +38,60 @@ const Dashboard = ({ dashboard }) => {
         })
         .catch((error) => console.error('Error fetching dashboard details:', error));
     }
-  }, [expanded, dashboard, dashboardDetails]);
+  }, [isExpanded, dashboard]);
+
+  const handleAccordionChange = () => {
+    onDashboardClick(dashboard.id);
+  };
 
   // Render individual dashboard item content with checks
   const renderDashboardItems = () => {
     if (dashboardDetails && dashboardDetails.dashboardItems) {
       return dashboardDetails.dashboardItems.map((item) => (
-        <div key={item.id}>
-          {item.visualization && item.visualization.name && (
-            <Typography>{item.visualization.name}</Typography>
+        <div key={item.id} style={{ display: 'flex', alignItems: 'center' }}>
+          {/* Conditional rendering based on item type */}
+          {item.visualization && item.visualization.type === 'COLUMN' && (
+            <DiagramIcon width={24} height={24} style={{ marginRight: '8px' }} />
           )}
-          {item.map && item.map.name && (
-            <Typography>{item.map.name}</Typography>
+          {item.visualization && item.visualization.type === 'PIVOT_TABLE' && (
+            <TableIcon width={24} height={24} style={{ marginRight: '8px' }} />
+          )}
+          {item.visualization && item.visualization.type === 'TEXT' && (
+            <TextIcon width={24} height={24} style={{ marginRight: '8px' }} />
+          )}
+          {item.visualization && item.visualization.type === 'MAP' && (
+            <MapIcon width={24} height={24} style={{ marginRight: '8px' }} />
+          )}
+          {item.visualization && item.visualization.type === 'STACKED_COLUMN' && (
+            <StatisticsIcon width={24} height={24} style={{ marginRight: '8px' }} />
+          )}
+          {item.visualization && item.visualization.type === 'PIE' && (
+            <StatisticsIcon width={24} height={24} style={{ marginRight: '8px' }} />
+          )}
+          {item.visualization && item.visualization.type === 'VISUALIZATION' && (
+            <DataVisIcon width={24} height={24} style={{ marginRight: '8px' }} />
           )}
           {/* Add similar checks for other item types */}
+          <div>
+            {item.visualization && item.visualization.name && (
+              <Typography>{item.visualization.name}</Typography>
+            )}
+            {item.map && item.map.name && (
+              <Typography>{item.map.name}</Typography>
+            )}
+            {/* Add similar checks for other item types */}
+          </div>
         </div>
       ));
     } else {
       return <Typography>Loading...</Typography>;
     }
   };
-  const handleAccordionChange = () => {
-    setExpanded(!expanded);
-  };
-  console.log('Dashboard details:', dashboardDetails)
+
+  console.log('Dashboard details:', dashboardDetails);
 
   return (
-    <Accordion expanded={expanded} onChange={handleAccordionChange}>
+    <Accordion expanded={isExpanded} onChange={handleAccordionChange}>
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
         aria-controls="panel1a-content"
